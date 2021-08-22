c692b7fec606b99adbdad8e78fc1b31eb2f8bd13
Cong <congusbongus@gmail.com>
2021-08-22 11:40:41 +1000

    Twine upload #2

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from distutils.core import setup
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+VERSION = "0.1.0"
 
 setup(
-    name="pykwalify-webform",
-    packages=["pykwalify-webform"],
-    version="0.1.0",
+    name="pykwalify_webform",
+    packages=find_packages(exclude=("tests",)),
+    version=VERSION,
     license="MIT",
     description="Generate webforms based on YAML schema with pykwalify",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="congusbongus",
     author_email="congusbongus@gmail.com",
     url="https://github.com/cxong/pykwalify-webform",
-    download_url="https://github.com/cxong/pykwalify-webform/archive/refs/tags/0.1.0.tar.gz",
+    download_url=f"https://github.com/cxong/pykwalify-webform/archive/refs/tags/{VERSION}.tar.gz",
     keywords=["pykwalify", "webform", "template"],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -19,4 +26,12 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
+    include_package_data=True,
+    install_requires=["jinja2", "pykwalify", "click"],
+    entry_points={
+        "console_scripts": [
+            "pykwalify_webform=pykwalify_webform.__main__:main",
+        ]
+    },
+    zip_safe=False,
 )
