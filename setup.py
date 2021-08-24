8513662ebb736ffea451db57a44109ee8d1538e7
Cong <congusbongus@gmail.com>
2021-08-24 20:20:06 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 setup(
     name="pykwalify_webform",
